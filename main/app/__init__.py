9a6ab07b3bc83136ba7dc1b19b367cccc01df6dc
Bartek Kachnic <bartek.kachnic123@gmail.com>
2023-05-22 20:50:52 +0200

    Dodano podstawowa web aplikacje

---
+++ main/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+def create_app(): 
+    app = Flask(__name__)
+
+    # Set config
+    app.config.from_object('config.Config')
+    
+    return app
+
+
+app = create_app()
+
+from app import views
