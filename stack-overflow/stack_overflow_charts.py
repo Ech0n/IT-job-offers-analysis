537a2784c7e9ef2804baefa09910f09b56af139e
Szymon Fortuna <131533540+szymonf123@users.noreply.github.com>
2023-06-03 12:10:23 +0200

    Posortowano legendę wykresu według poziomów wykształcenia
    Próbowałem też pobawić się trochę kolorystyką wykresu, aby kolory poszczególnych serii były w tym samym odcieniu , ale żeby wraz z wyższym poziomem zmieniać saturację / jasność (bo te poziomy są uporządkowane, więc miałoby to logiczny sens - tak też było mówione na wykładzie), jednak tych poziomów jest na to trochę za dużo i te kolory były zbyt podobne do siebie.

--- stack-overflow/stack_overflow_charts.py
+++ stack-overflow/stack_overflow_charts.py
@@ -5,20 +5,17 @@ def degrees_through_years():
     df = pd.read_csv("/content/drive/MyDrive/Wizualizacja danych/stack-overflow/survey_results_public_2017.csv")
     poland_data = df[df["Country"] == "Poland"]
     levels2017 = poland_data["FormalEducation"].value_counts()
-    #levels2017["Associate degree"] = 0
     levels2017["NDoctoral degree"] = levels2017["Doctoral degree"]
     levels2017["Something else"] = levels2017["I prefer not to answer"]
     levels2017 = levels2017.drop(labels=["Doctoral degree", "I prefer not to answer"])
     levels2017 = levels2017.sort_index()
-    #print(levels2017.values)
-    
+
     df = pd.read_csv("/content/drive/MyDrive/Wizualizacja danych/stack-overflow/survey_results_public_2018.csv")
     poland_data = df[df["Country"] == "Poland"]
     levels2018 = poland_data["FormalEducation"].value_counts()
     levels2018["Something else"] = levels2018["Associate degree"]
     levels2018 = levels2018.drop(labels=["Associate degree"])
     levels2018 = levels2018.sort_index()
-    #print(levels2018.values)
 
     df = pd.read_csv("/content/drive/MyDrive/Wizualizacja danych/stack-overflow/survey_results_public_2019.csv")
     poland_data = df[df["Country"] == "Poland"]
@@ -26,7 +23,6 @@ def degrees_through_years():
     levels2019["Something else"] = levels2019["Associate degree"]
     levels2019 = levels2019.drop(labels=["Associate degree"])
     levels2019 = levels2019.sort_index()
-    #print(levels2019.values)
 
     df = pd.read_csv("/content/drive/MyDrive/Wizualizacja danych/stack-overflow/survey_results_public_2020.csv")
     poland_data = df[df["Country"] == "Poland"]
@@ -34,7 +30,6 @@ def degrees_through_years():
     levels2020["Something else"] = levels2020["Associate degree (A.A., A.S., etc.)"]
     levels2020 = levels2020.drop(labels=["Associate degree (A.A., A.S., etc.)"])
     levels2020 = levels2020.sort_index()
-    #print(levels2020.values)
 
     df = pd.read_csv("/content/drive/MyDrive/Wizualizacja danych/stack-overflow/survey_results_public_2021.csv")
     poland_data = df[df["Country"] == "Poland"]
@@ -43,7 +38,6 @@ def degrees_through_years():
     levels2021["Something else"] += levels2021["Associate degree (A.A., A.S., etc.)"]
     levels2021 = levels2021.drop(labels=["Associate degree (A.A., A.S., etc.)"])
     levels2021 = levels2021.sort_index()
-    #print(levels2021.values)
 
     df = pd.read_csv("/content/drive/MyDrive/Wizualizacja danych/stack-overflow/survey_results_public_2022.csv")
     poland_data = df[df["Country"] == "Poland"]
@@ -52,7 +46,6 @@ def degrees_through_years():
     levels2022["Something else"] += levels2022["Associate degree (A.A., A.S., etc.)"]
     levels2022 = levels2022.drop(labels=["Associate degree (A.A., A.S., etc.)"])
     levels2022 = levels2022.sort_index()
-    #print(levels2022.values)
 
     years = [2017, 2018, 2019, 2020, 2021, 2022]
     data = []
@@ -65,11 +58,13 @@ def degrees_through_years():
       data[i].append(levels2021.values[i])
       data[i].append(levels2022.values[i])
     
-    keys = ["Licencjackie / inżynierskie", "Brak", "Magisterskie", "Doktorskie", "Podstawowe", "Profesorskie (lub dr hab.)", "Średnie", "Nieukończone studia", "Inne"]
+    keys = ["Licencjackie / inżynierskie", "Brak", "Magisterskie", "Doktorskie", "Podstawowe", "Wyższe niż doktorskie", "Średnie", "Nieukończone studia", "Inne"]
     df = pd.DataFrame()
     for i in range(9):
       df[keys[i]] = data[i]
 
+    keys = ["Brak", "Podstawowe", "Średnie", "Nieukończone studia", "Licencjackie / inżynierskie", "Magisterskie", "Doktorskie", "Wyższe niż doktorskie", "Inne"]
+    df = df[keys]
     df = df.divide(df.sum(axis=1), axis=0)
     plt.stackplot(years, df.values.T, labels=keys)
     plt.title("Udział w rynku osób z poszczególnym wykształceniem w latach 2017-2022")
