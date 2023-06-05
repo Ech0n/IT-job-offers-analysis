16fdb93223658ed2b8615163a7781c6d9ffc29a0
Dawid <134408390+Dawid-Augustynowicz@users.noreply.github.com>
2023-06-05 19:21:10 +0200

    Wczytywanie danych na początku programu

--- stack-overflow/stack_overflow_charts.py
+++ stack-overflow/stack_overflow_charts.py
@@ -4,47 +4,53 @@ import matplotlib.pyplot as plt
 # data_source = "/content/drive/MyDrive/Wizualizacja danych/stack-overflow/"
 data_source = "../data/stack-overflow/"
 
+"""df2013 = pd.read_csv(data_source + "2013 Stack Overflow Survey Responses.csv", low_memory=False)
+df2014 = pd.read_csv(data_source + "2014 Stack Overflow Survey Responses.csv")
+df2015 = pd.read_csv(data_source + "2015 Stack Overflow Developer Survey Responses.csv", low_memory=False)
+df2016 = pd.read_csv(data_source + "2016 Stack Overflow Survey Responses.csv")"""
+
+df2017 = pd.read_csv(data_source + "survey_results_public_2017.csv")
+df2017 = df2017[df2017["Country"] == "Poland"]
+df2018 = pd.read_csv(data_source + "survey_results_public_2018.csv", low_memory=False)
+df2018 = df2018[df2018["Country"] == "Poland"]
+df2019 = pd.read_csv(data_source + "survey_results_public_2019.csv")
+df2019 = df2019[df2019["Country"] == "Poland"]
+df2020 = pd.read_csv(data_source + "survey_results_public_2020.csv")
+df2020 = df2020[df2020["Country"] == "Poland"]
+df2021 = pd.read_csv(data_source + "survey_results_public_2021.csv")
+df2021 = df2021[df2021["Country"] == "Poland"]
+df2022 = pd.read_csv(data_source + "survey_results_public_2022.csv")
+df2022 = df2022[df2022["Country"] == "Poland"]
+
 def degrees_through_years():
-    df = pd.read_csv(data_source + "survey_results_public_2017.csv")
-    poland_data = df[df["Country"] == "Poland"]
-    levels2017 = poland_data["FormalEducation"].value_counts()
+    levels2017 = df2017["FormalEducation"].value_counts()
     levels2017["NDoctoral degree"] = levels2017["Doctoral degree"]
     levels2017["Something else"] = levels2017["I prefer not to answer"]
     levels2017 = levels2017.drop(labels=["Doctoral degree", "I prefer not to answer"])
     levels2017 = levels2017.sort_index()
 
-    df = pd.read_csv(data_source + "survey_results_public_2018.csv", dtype="unicode")
-    poland_data = df[df["Country"] == "Poland"]
-    levels2018 = poland_data["FormalEducation"].value_counts()
+    levels2018 = df2018["FormalEducation"].value_counts()
     levels2018["Something else"] = levels2018["Associate degree"]
     levels2018 = levels2018.drop(labels=["Associate degree"])
     levels2018 = levels2018.sort_index()
 
-    df = pd.read_csv(data_source + "survey_results_public_2019.csv")
-    poland_data = df[df["Country"] == "Poland"]
-    levels2019 = poland_data["EdLevel"].value_counts()
+    levels2019 = df2019["EdLevel"].value_counts()
     levels2019["Something else"] = levels2019["Associate degree"]
     levels2019 = levels2019.drop(labels=["Associate degree"])
     levels2019 = levels2019.sort_index()
 
-    df = pd.read_csv(data_source + "survey_results_public_2020.csv")
-    poland_data = df[df["Country"] == "Poland"]
-    levels2020 = poland_data["EdLevel"].value_counts()
+    levels2020 = df2020["EdLevel"].value_counts()
     levels2020["Something else"] = levels2020["Associate degree (A.A., A.S., etc.)"]
     levels2020 = levels2020.drop(labels=["Associate degree (A.A., A.S., etc.)"])
     levels2020 = levels2020.sort_index()
 
-    df = pd.read_csv(data_source + "survey_results_public_2021.csv")
-    poland_data = df[df["Country"] == "Poland"]
-    levels2021 = poland_data["EdLevel"].value_counts()
+    levels2021 = df2021["EdLevel"].value_counts()
     levels2021["I have never completed any formal education"] = 0
     levels2021["Something else"] += levels2021["Associate degree (A.A., A.S., etc.)"]
     levels2021 = levels2021.drop(labels=["Associate degree (A.A., A.S., etc.)"])
     levels2021 = levels2021.sort_index()
 
-    df = pd.read_csv(data_source + "survey_results_public_2022.csv")
-    poland_data = df[df["Country"] == "Poland"]
-    levels2022 = poland_data["EdLevel"].value_counts()
+    levels2022 = df2022["EdLevel"].value_counts()
     levels2022["I have never completed any formal education"] = 0
     levels2022["Something else"] += levels2022["Associate degree (A.A., A.S., etc.)"]
     levels2022 = levels2022.drop(labels=["Associate degree (A.A., A.S., etc.)"])
