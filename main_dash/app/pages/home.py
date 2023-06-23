5cb5793aa3104b3c843db8d9c19796c279965797
Mateusz Dyszewski <43513980+Ech0n@users.noreply.github.com>
2023-06-23 05:05:00 +0200

    Add files via upload

--- main_dash/app/pages/home.py
+++ main_dash/app/pages/home.py
@@ -4,20 +4,10 @@ from dash import html, dcc
 dash.register_page(__name__, path='/')
 
 layout = html.Div(children=[
-    html.H1(children='Analiza branży informatycznej w Polsce'),
+    html.H1(children='This is our Home page'),
 
     html.Div(children='''
-        W ramach projektu z zajęć z wizualizacji danych przygotowaliśmy graficzną analizę danych dotyczącą branży informatycznej oraz informatyków w Polsce.
-        Jest ona podzielona na trzy części:
+        This is our Home page content.
     '''),
 
-    html.H3(children='1. Analiza ofert pracy dla informatyków'),
-
-    html.Div(children='Tutaj wykorzystano dane z portalu PRACUJ.PL z lat 2018-2023'),
-
-    html.H3(children='2. Analiza działalności zawodowej informatyków'),
-
-    html.Div(children='''Wykorzystano tutaj dane z dwóch źródeł: STACK OVERFLOW, gdzie od 2011 r. programiści wypełniają ankiety dotyczące ich działalności, zatrudnienia i 
-             umiejętności. Polacy uczestniczą w tych ankietach od 2014 r., jednak co roku rubryki w ankietach zmieniają się. Drugim źródłem danych jest GITHUB,
-             gdzie znajdziemy statystyki na temat repozytoriów.'''),
 ])
--- main_dash/app/pages/pracuj.py
+++ main_dash/app/pages/pracuj.py
@@ -1,6 +1,7 @@
 import dash
 from dash import html
 from app import pracuj_layouts
+import dash_bootstrap_components as dbc
 
 
 dash.register_page(__name__)
@@ -13,20 +14,23 @@ layout = html.Div(children=[
         #html.H1(children='', style={"height": "450px"}),
         #html.Div(children=tree_chart_github.create_chart2())
         html.Div(children=pracuj_layouts.create_chart2(), style={"min-height" : "450px"}),
-        html.Div(children=pracuj_layouts.create_chart_degree, style={"min-height" : "450px"}),
-        html.Div(children=pracuj_layouts.create_chart_driving_license, style={"min-height" : "450px"}),
-        html.Div(children=pracuj_layouts.liczba_ofert, style={"min-height" : "450px"}),
+        # dbc.Row([
+        #     html.Div(children=pracuj_layouts.create_chart_driving_license, style={"min-height" : "450px",'width':"50%"}),
+        # ]),
+        html.Div(children=pracuj_layouts.liczba_ofert, style={"min-height" : "550px"}),
+        html.Div(children=pracuj_layouts.offers_by_region, style={"min-height" : "650px","width":"100%"}),
+
         html.Div(children=pracuj_layouts.types_of_contract, style={"min-height" : "450px"}),
         html.Div(children=pracuj_layouts.experience_level, style={"min-height" : "450px"}),
         # html.Div(children=pracuj_layouts.benefits_dynamic(), style={"min-height" : "450px"}),
         html.Div(children=pracuj_layouts.diffrent_benefits, style={"min-height" : "450px"}),
         html.Div(children=pracuj_layouts.salaries, style={"min-height" : "450px"}),
-        html.Div(children=pracuj_layouts.salary_by_region, style={"min-height" : "450px"}),
-        # html.Div(children=pracuj_layouts.requirements, style={"min-height" : "450px"}),
-
+        html.Div(children=pracuj_layouts.salary_by_region, style={"min-height" : "650px"}),
+        html.Div(children=pracuj_layouts.salary_trend, style={"min-height" : "450px"}),
         # html.Div(children=pracuj_layouts.offers_by_region, style={"min-height" : "450px"}),
-    
-        
+        html.Div(children=pracuj_layouts.create_chart_degree, style={"min-height" : "450px",'width':"100%"}),
+        html.Div(children=pracuj_layouts.other_requirements, style={"min-height" : "450px"}),
+        html.Div(children=pracuj_layouts.requirements, style={"min-height" : "800px"}),
 ])
 
 
